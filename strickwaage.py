baa353cc07241ccbeccecd34e37283ce84f9a3f1
weitzenberger <lennart29.9@gmail.com>
2021-02-26 23:11:52 +0100

    Extensionboard

--- strickwaage.py
+++ strickwaage.py
@@ -23,13 +23,11 @@ import time
 SCALES = {
     1: {  # Waagennummer als Key-Value
         "hx711": {
-            "dout_pin": 0b00000001,  # Raspberry PI DOUT_PIN Nummer oder wenn "device_adress" definiert Binärcode für den PIN im Extensionboard
-            "pd_sck_pin": 0b00000001,  # Raspberry PI PD_SCK_PIN Nummer
+            "dout_pin": 0,  # Raspberry PI DOUT_PIN Nummer oder wenn "device_adress" definiert Binärcode für den PIN im Extensionboard
+            "pd_sck_pin": 8,  # Raspberry PI PD_SCK_PIN Nummer
             "gain_channel_A": 128,  # Der gain ist optional. Default: 128
             "select_channel": 'A',  # Der Channel ist optional. Default: 'A'
             "device_address": 0x27,  # Device Adresse von dem ExpansionBoard in Hexadezimal
-
-
         },
         "offset": 107154,  # Offset wird mithilfe von calibrate-cli.py berechnet
         "ratio": 513.8683333333333  # ratio wird mithilfe von calibrate-cli.py berechnet
