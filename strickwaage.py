298939417fb43edf0daa725a28521b6b7f430843
weitzenberger <lennart29.9@gmail.com>
2021-03-08 20:22:33 +0100

    Extensionboard

--- strickwaage.py
+++ strickwaage.py
@@ -23,8 +23,8 @@ import time
 SCALES = {
     1: {  # Waagennummer als Key-Value
         "hx711": {
-            "dout_pin": 0,  # Raspberry PI DOUT_PIN Nummer oder wenn "device_adress" definiert Binärcode für den PIN im Extensionboard
-            "pd_sck_pin": 8,  # Raspberry PI PD_SCK_PIN Nummer
+            "dout_pin": 2,  # Raspberry PI DOUT_PIN Nummer oder wenn "device_adress" definiert Binärcode für den PIN im Extensionboard
+            "pd_sck_pin": 6,  # Raspberry PI PD_SCK_PIN Nummer
             "gain_channel_A": 128,  # Der gain ist optional. Default: 128
             "select_channel": 'A',  # Der Channel ist optional. Default: 'A'
             "device_address": 0x27,  # Device Adresse von dem ExpansionBoard in Hexadezimal
@@ -34,10 +34,25 @@ SCALES = {
     },
     2: {  # Waagennummer als Key-Value
         "hx711": {
-            "dout_pin": 5,# Raspberry PI DOUT_PIN Nummer oder wenn "device_adress" definiert Binärcode für den PIN im Extensionboard
+            "dout_pin": 1,
+            # Raspberry PI DOUT_PIN Nummer oder wenn "device_adress" definiert Binärcode für den PIN im Extensionboard
             "pd_sck_pin": 6,  # Raspberry PI PD_SCK_PIN Nummer
             "gain_channel_A": 128,  # Der gain ist optional. Default: 128
             "select_channel": 'A',  # Der Channel ist optional. Default: 'A'
+            "device_address": 0x27,  # Device Adresse von dem ExpansionBoard in Hexadezimal
+
+        },
+        "offset": 107154,  # Offset wird mithilfe von calibrate-cli.py berechnet
+        "ratio": 513.8683333333333  # ratio wird mithilfe von calibrate-cli.py berechnet
+    },
+    3: {  # Waagennummer als Key-Value
+        "hx711": {
+            "dout_pin": 0,
+        # Raspberry PI DOUT_PIN Nummer oder wenn "device_adress" definiert Binärcode für den PIN im Extensionboard
+            "pd_sck_pin": 6,  # Raspberry PI PD_SCK_PIN Nummer
+            "gain_channel_A": 128,  # Der gain ist optional. Default: 128
+            "select_channel": 'A',  # Der Channel ist optional. Default: 'A'
+            "device_address": 0x27,  # Device Adresse von dem ExpansionBoard in Hexadezimal
 
         },
         "offset": 107154,  # Offset wird mithilfe von calibrate-cli.py berechnet
