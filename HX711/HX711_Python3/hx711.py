58b888706852a25330627c2a54263abdfe1496fc
weitzenberger <lennart29.9@gmail.com>
2021-03-08 21:23:46 +0100

    Extensionboard

--- HX711/HX711_Python3/hx711.py
+++ HX711/HX711_Python3/hx711.py
@@ -6,12 +6,10 @@ This file holds HX711 class
 import statistics as stat
 import time
 
-import RPi.GPIO as GPIO
 
 
 
-import wiringpi2 as wiringpi
-import smbus
+import wiringpi
 
 IODIRA = 0x00  # Register In/Out für A
 IODIRB = 0x10  # Register In/Out für B
@@ -57,7 +55,7 @@ class HX711:
         self._scale_ratio_A_128 = 1  # scale ratio for channel A and gain 128
         self._scale_ratio_A_64 = 1  # scale ratio for channel A and gain 64
         self._scale_ratio_B = 1  # scale ratio for channel B
-        self._debug_mode = True
+        self._debug_mode = False
         self._data_filter = outliers_filter  # default it is used outliers_filter
         self.device_adress_dout = device_address_dout
         self.pin_base = pin_base
